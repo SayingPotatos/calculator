ecba113fe6c773ef23bb8f8381b3fa1ec2995bb6
Daeuning <126398267+Daeuning@users.noreply.github.com>
2023-11-14 17:32:42 +0900

    Update easteregg.py

--- easteregg.py
+++ easteregg.py
@@ -1,48 +1,28 @@
 # TODO: 특정 숫자가 입력되었는지 판별 후, 맞으면 이스터에그 표시
-import copy
 
 def easteregg(args):
-    _args = copy.deepcopy(args)
-    
-    if _args == 1126611:
-        return '사랑해'
-    elif _args == 9090:
-        return '가자'
-    elif _args == 982:
-        return '잘 가'
-    elif _args == 827:
-        return '파이팅'
-    elif _args == 11010:
-        return '흥'
-    elif _args == 825:
-        return '빨리 와'
-    elif _args == 2222:
-        return '투덜투덜'
-    elif _args == 505:
-        return 'SOS'
-    elif _args == 230:
-        return '이상 없음'
-    elif _args == 100:
-        return '돌아와'
-    elif _args == 175:
-        return '일찍 와'
-    elif _args == 981:
-        return '급한 일이 생겼어'
-    elif _args == 337:
-        return '힘내'
-    elif _args == 1004:
-        return '천사'
-    elif _args == 9431:
-        return '구사일생'
-    elif _args == 1414:
-        return '밥먹자'
-    elif _args == 952:
-        return '좋은 아침'
-    elif _args == 1008:
-        return '고민 중'
-    elif _args == 1225:
-        return 'MerryChristmas!'
-    else:
-        return 0
+    dictionary = {
+            1126611: '사랑해',
+            9090: '가자',
+            982: '잘 가',
+            827: '파이팅',
+            11010: '흥',
+            825: '빨리 와',
+            2222: '투덜투덜',
+            505: 'SOS',
+            230: '이상 없음',
+            100: '돌아와',
+            175: '일찍 와',
+            981: '급한 일이 생겼어',
+            337: '힘내',
+            1004: '천사',
+            9431: '구사일생',
+            1414: '밥먹자',
+            952: '좋은 아침',
+            1008: '고민 중',
+            1225: 'Merry Christmas!',
+        }
+        
+    return dictionary.get(args, 0)
 
 
