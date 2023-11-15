53c0e2b9cf0de3f65bfb8dd35d64b2e8ef920400
sksla123 <pjy010608@naver.com>
2023-11-15 17:25:29 +0900

    fix: validator
    전에는 원본 스택에 영향을 안주려고 깊은 복사를 수행하고 int()로 검증했는데 지금은 원본 내부의 문자열을 그냥 int형으로 변경하도록 바꿈

--- error.py
+++ error.py
@@ -1,25 +1,21 @@
 # TODO: 정상적인 수식인지 확인 및 에러 시 동작
-import copy
 
 # 0 반환시 ERROR
 # 1 반환시 계산 수행
-def validator(args):    
-    # 원본 변수에 영향을 주지 않기 위해 깊은 복사를 수행    
-    _args = copy.deepcopy(args)
-    
+def validator(args):        
     # 정해진 연산자
     ops = ['+', '-', 'x', '=']
     
-    for i in range(len(_args)):
+    for i in range(len(args)):
         if (i % 2 == 0):
             try:
-                _args[i] = int(_args[i])
+                args[i] = int(args[i])
             except ValueError:
                 # 정수가 입력되어야 하는데 정수 형태의 입력이 아님
                 return 0
         else:
             # 정해진 연산자 이외의 입력이 들어옴
-            if _args[i] not in ops:
+            if args[i] not in ops:
                 return 0
     
     return 1
