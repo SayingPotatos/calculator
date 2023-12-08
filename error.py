06d2de42f58f41646bcd7961b2f908363c87cfcc
sksla123 <pjy010608@naver.com>
2023-12-08 16:05:33 +0900

    #fix 캐쉬 파일 삭제, 구 validator 삭제, 잔존 폴더 삭제
    operator 폴더가 남아있어서 삭제했습니다.

--- error.py
+++ error.py
@@ -46,28 +46,3 @@ def validator(state, st, u):
         u = int(u)
     
     return state, u
-
-  # 0 반환시 ERROR
-# 1 반환시 계산 수행
-def validator(args):        
-    # 정해진 연산자
-    ops = ['+', '-', 'x', '=']
-    
-    for i in range(len(args)):
-        if (i % 2 == 0):
-            try:
-                args[i] = int(args[i])
-            except ValueError:
-                # 정수가 입력되어야 하는데 정수 형태의 입력이 아님
-                return 0
-        else:
-            # 정해진 연산자 이외의 입력이 들어옴
-            if args[i] not in ops:
-                return 0
-    
-    return 1
-
-def noiseFilter(args):
-    # 원소 내부의 공백 제거
-    for i in range(len(args)):
-        args[i] = args[i].strip();
