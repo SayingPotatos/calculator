ab75c08a66ba951433d0d9ff79b6f44a17298347
yaehee <yaehee@kakao.com>
2023-12-08 20:20:01 +0900

    feat: 테스트 코드베이스 작성

---
+++ tests/test_multiplication.py
@@ -0,0 +1,11 @@
+import unittest as ut
+
+# test하고 싶은 메소드 import
+#---------------------------Factorial test-----------------------------
+from operators.multiplication import multiplication
+
+# test class
+#----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    # 러닝 코드용
