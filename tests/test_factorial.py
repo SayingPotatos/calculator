d4a23347431b200871050ec4c999a7b13c7b9c9b
강경빈 <kid5107@naver.com>
2023-12-09 20:09:49 +0900

    feat : test1추가 #18

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -6,6 +6,9 @@ from operators.factorial import factorial
 
 # test class
 #----------------------------------------------------------------------
-
+class TestFactorial(ut.TestCase):
+    def test_1(self): 
+        self.assertEqual(factorial(-1),"[ERROR] Out Of Range") #음수 입력시 에러 코드 
+        
 if __name__ == '__main__':
     ut.main()
