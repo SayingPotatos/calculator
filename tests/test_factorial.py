5ef5fb6f4ec039e3634a1ad054a19d4e23fa0776
yaehee <yaehee@kakao.com>
2023-12-09 20:55:14 +0900

    fix: merge factorial code

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -5,7 +5,10 @@ import unittest as ut
 from operators.factorial import factorial
 
 # test class
-#---------------------------------------------------------------------
+#----------------------------------------------------------------------
+class TestFactorial(ut.TestCase):
+    def test_1(self): 
+        self.assertEqual(factorial(-1),"[ERROR] Out Of Range") #음수 입력시 에러 코드 
         
 if __name__ == '__main__':
-    ut.main()
+    # 러닝 코드용
