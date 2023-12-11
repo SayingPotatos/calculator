e7a4f02fdb62b93e71eb64fda201a42566d121c6
Jeong Daeun <126398267+Daeuning@users.noreply.github.com>
2023-12-11 21:53:20 +0900

    Update factorial

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -21,17 +21,26 @@ class TestFactorial_Input0(ut.TestCase):
         test_input = 0
         test_output = 1
         self.assertEqual(test_output, factorial(test_input))
-#----------------------------------------------------------------------
+
+class TestFactorial_InputNormal(ut.TestCase):
+    def test_4(self):
+        test_input = 3
+        test_output = 6
+        self.assertEqual(test_output, factorial(test_input))
+        
         
 if __name__ == '__main__':
     factorial_inputLess0_suite = ut.TestSuite()
     factorial_inputLess0_suite.addTests(ut.TestLoader().loadTestsFromTestCase(TestFactorial_InputLess0))
-    
+
     factorial_input0_suite = ut.TestSuite()
     factorial_input0_suite.addTests(ut.TestLoader().loadTestsFromTestCase(TestFactorial_Input0))
 
+    factorial_inputNormal_suite = ut.TestSuite()
+    factorial_inputNormal_suite.addTests(ut.TestLoader().loadTestsFromTestCase(TestFactorial_InputNormal))
+
     # 팩토리얼 관련 suite들을 하나로 묶음
-    all_factorial_suites = ut.TestSuite([factorial_inputLess0, factorial_input0_suite])
-    
+    all_factorial_suites = ut.TestSuite([factorial_inputLess0_suite, factorial_input0_suite, factorial_inputNormal_suite])
+
     # 팩토리얼 suites에 대한 test실행
     ut.TextTestRunner().run(all_factorial_suites)
