d0fd590371cabdd8dd5179d7f9b9f5f018726d72
kangkyungbin <103020046+kangkyeongbin@users.noreply.github.com>
2023-12-13 00:40:31 +0900

    fix : "x"삽입

--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -5,18 +5,21 @@ from calculator import calculator
 class TestMultiplication(ut.TestCase):
     def test_multiplication_one(self):
         self.assertEqual(multiplication(1, 2), 2)
+        self.assertEqual(calculator([1, "x", 2]), 2)
         self.assertEqual(calculator([1, "*", 2]), 2)
-        self.assertEqual(calculator(["1", "*", "2"]), 2)
+        self.assertEqual(calculator(["1", "x", "2"]), 2)
 
     def test_multiplication_zero(self):
         self.assertEqual(multiplication(0, 0), 0)
+        self.assertEqual(calculator([0, "x", 0]), 0)
         self.assertEqual(calculator([0, "*", 0]), 0)
-        self.assertEqual(calculator(["0", "*", "0"]), 0)
+        self.assertEqual(calculator(["0", "x", "0"]), 0)
 
     def test_multiplication_large_numbers(self):
         self.assertEqual(multiplication(1000000, 2000000), 2000000000000)
+        self.assertEqual(calculator([1000000, "x", 2000000]), 2000000000000)
         self.assertEqual(calculator([1000000, "*", 2000000]), 2000000000000)
-        self.assertEqual(calculator(["1000000", "*", "2000000"]), 2000000000000)
+        self.assertEqual(calculator(["1000000", "x", "2000000"]), 2000000000000)
 
 def suite():
     test_suite = ut.TestSuite()
