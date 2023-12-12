a6b4e63852b3f6586fc3e458b91a8554513f6e1e
kangkyungbin <103020046+kangkyeongbin@users.noreply.github.com>
2023-12-13 00:35:41 +0900

    fix : main
    이스터에그 출력부분 [EVENT] 추가

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ while user_input != "=":
     
     easter = easteregg(user_input)
     if easter != 0:
-        print("EVENT",easter)
+        print("[EVENT]",easter)
 
     if state != 3:
         state, user_input = validator(state, user_input)
